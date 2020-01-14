a916d963cb20e00ca8f2ee41212f49e922034cb8
Matthias Schiffer <matthias.schiffer@tq-group.com>
2020-01-14 11:09:31 +0100

    emit: replace built-in compatible check with custom hook
    Signed-off-by: Matthias Schiffer <matthias.schiffer@tq-group.com>

--- recipes-devtools/emit/files/hook.sh
+++ recipes-devtools/emit/files/hook.sh
@@ -1,44 +1,67 @@
 #!/bin/sh
 #
+# Copyright (C) 2020, TQ Systems
+#
+# slot-install handling is:
+#
 # (c) Gateware Communications GmbH, Nuremberg 2018
 #
 
 set -eo pipefail
 
-echo "i.MX28 bootloader install script"
 
-if [ "$1" != "slot-install" ]; then
-	echo "Invalid action '$1'! Aborting." >&2
-	exit 1
-fi
-if [ "$RAUC_SLOT_CLASS" != "u-boot" ]; then
-	echo "Invalid slot class '$RAUC_SLOT_CLASS'! Aborting" >&2
-	exit 1
-fi
+case "$1" in
 
-BOOT_SLOT="$(imx28-blupdate status --device "$RAUC_SLOT_DEVICE" --boot-source | awk '/NOTICE: Boot source: / {print $4}')"
+install-check)
+	if [ "$RAUC_MF_COMPATIBLE" != "$RAUC_SYSTEM_COMPATIBLE" ]; then
+		echo "Compatible does not match!" >&2
+		exit 10
+	fi
 
-case "$BOOT_SLOT" in
-Primary)
-	SLOTS='second first'
 	;;
-Secondary)
-	SLOTS='first second'
-	;;
-*)
-	echo "Unable to determine boot slot! Aborting." >&2
-	exit 1
-esac
 
-for slot in $SLOTS; do
-	INSTALLED=$( \
-		imx28-blupdate extract --device "$RAUC_SLOT_DEVICE" --$slot - \
-		| sha256sum | awk '{print $1}' || true \
-	)
-	if [ "$INSTALLED" == "$RAUC_IMAGE_DIGEST" ]; then
-		echo "Bootloader in $slot slot is already up to date! Skipping."
-	else
+slot-install)
+	if [ "$RAUC_SLOT_CLASS" != "u-boot" ]; then
+		echo "Invalid slot class '$RAUC_SLOT_CLASS'! Aborting" >&2
+		exit 1
+	fi
+
+	BOOT_SLOT="$(imx28-blupdate status --device "$RAUC_SLOT_DEVICE" --boot-source | awk '/NOTICE: Boot source: / {print $4}')"
+
+	case "$BOOT_SLOT" in
+	Primary)
+		SLOTS='second first'
+		;;
+	Secondary)
+		SLOTS='first second'
+		;;
+	*)
+		echo "Unable to determine boot slot! Aborting." >&2
+		exit 1
+	esac
+
+	for slot in $SLOTS; do
+		INSTALLED=$( \
+			imx28-blupdate extract --device "$RAUC_SLOT_DEVICE" --$slot - \
+			| sha256sum | awk '{print $1}' || true \
+		)
+		if [ "$INSTALLED" = "$RAUC_IMAGE_DIGEST" ]; then
+			echo "Bootloader in slot $slot is already up to date! Skipping."
+			continue
+		fi
+
+		echo "Updating bootloader in slot $slot"
+
 		imx28-blupdate upgrade --device "$RAUC_SLOT_DEVICE" --$slot "$RAUC_IMAGE_NAME"
 		imx28-blupdate status --device "$RAUC_SLOT_DEVICE" --$slot
-	fi
-done
+	done
+
+	;;
+
+*)
+	exit 1
+	;;
+
+esac
+
+exit 0
