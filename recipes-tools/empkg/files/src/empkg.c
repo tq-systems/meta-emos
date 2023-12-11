d990f48ee2acb121ad8ce0f9ed0a1ed36a7267c7
Michael Krummsdorf <michael.krummsdorf@tq-group.com>
2023-12-11 10:25:49 +0100

    empkg: add app_enable function
    Signed-off-by: Michael Krummsdorf <michael.krummsdorf@tq-group.com>

--- recipes-tools/empkg/files/src/empkg.c
+++ recipes-tools/empkg/files/src/empkg.c
@@ -9,6 +9,7 @@
 #include "em-app-generator.h"
 #include "empkg.h"
 #include "empkg_appdb.h"
+#include "empkg_enable.h"
 #include "empkg_info.h"
 #include "empkg_install.h"
 #include "empkg_list.h"
---
+++ recipes-tools/empkg/files/src/empkg_enable.h
@@ -0,0 +1,10 @@
+/*
+ * empkg_enable.h - App enable header
+ *
+ * Copyright © 2024 TQ-Systems GmbH <info@tq-group.com>
+ * All rights reserved. For further information see LICENSE.
+ * Author: Michael Krummsdorf
+ */
+
+int empkg_enable(const char *id);
+int app_enable(const char *id);
