a801754cb0b6400055157931c1ebe15c1dd9311e
Michael Krummsdorf <michael.krummsdorf@tq-group.com>
2024-08-13 09:35:18 +0200

    em-app-generator: messages to kernel log
    Make user info to update apps more visible
    in logs by putting them into kernel log (dmesg).
    
    Signed-off-by: Michael Krummsdorf <michael.krummsdorf@tq-group.com>

--- recipes-tools/empkg/files/src/em-app-generator.c
+++ recipes-tools/empkg/files/src/em-app-generator.c
@@ -50,6 +50,28 @@ const char *const TIME_APPS[] = {
 	NULL,
 };
 
+/* write log message to stderr and also to kernel log (requires root) */
+void log_message(const char *format, ...) {
+	FILE *kmsg;
+	va_list args;
+	va_start(args, format);
+
+	// Check if the process is running as root (UID 0)
+	if (geteuid() == 0) {
+		kmsg = fopen("/dev/kmsg", "a");
+		if (kmsg != NULL) {
+			vfprintf(kmsg, format, args);
+			fclose(kmsg);
+		} else {
+			fprintf(stderr, "Error: Unable to open /dev/kmsg\n");
+		}
+	}
+
+	vfprintf(stderr, format, args);
+
+	va_end(args);
+}
+
 char *empkg_json_get_char(const char *app, const char *key) {
 	const char *manifest_pattern = "/apps/installed/%s/manifest.json";
 	json_t *json;
@@ -89,7 +111,7 @@ bool is_core_app(const char *app) {
 		return false;
 	}
 
-	fprintf(stderr, "Property 'appclass' not found in manifest. Please update %s by 2025.\n", app);
+	log_message("Property 'appclass' not found in manifest. Please update %s by 2025.\n", app);
 
 	for (i = 0; CORE_APPS[i]; i++) {
 		if (strcmp(app, CORE_APPS[i]) == 0)
